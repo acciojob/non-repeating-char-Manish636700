899c1ac23d8d423cf255e917a3aa839068b40936
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 04:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ if(typeof str !== 'String' || str.length === 0) 
+	 return "";
+
+	let count={};
+
+	for(let a of str){
+		count[a] === (const[a] || 0)+1;
+	}
+
+	for(let a of str)
+		{
+			if(count[a]===1)
+				return a;
+		}
+	return "";
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
